11d8669f006c48d4ccccd2eb936c22855167cbaf
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2019-03-22 15:17:04 +0200

    WIP

--- Tests/Orleankka.Tests/Features/Background_jobs.cs
+++ Tests/Orleankka.Tests/Features/Background_jobs.cs
@@ -1,41 +1,111 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
 
 namespace Orleankka.Features
 {
     namespace Background_jobs
     {
+        using Utility;
         using Testing;
 
         [TestFixture]
         [RequiresSilo]
         public class Tests
         {
+            BackgroundJobService jobs;
+
+            [SetUp]
+            public void SetUp()
+            {
+                jobs = new BackgroundJobService();
+            }
+
             [Test]
             public void When_no_job_is_running()
             {
-                var jobs = new BackgroundJobService();
                 Assert.That(jobs.IsRunning("foo"), Is.False);
                 Assert.That(jobs["foo"], Is.Null);
                 Assert.That(jobs.Count,  Is.EqualTo(0));
+            }
+
+            [Test]
+            public async Task When_job_is_running()
+            {
+                var cancellation = new CancellationTokenSource();
+                
+                async Task Work(BackgroundJobTerminationToken _)
+                {
+                    while (!cancellation.IsCancellationRequested)
+                        await Task.Delay(TimeSpan.FromMilliseconds(100), cancellation.Token);
+                }
+
+                var task = Task.Run(() => jobs.Run("foo", Work), cancellation.Token);
+
+                Assert.That(jobs.IsRunning("foo"), Is.True);
+                Assert.That(jobs["foo"], Is.Not.Null);
+                Assert.That(jobs.Count,  Is.EqualTo(1));
+
+                var job = jobs["foo"];
+                Assert.That(job.IsRunning, Is.True);
+                Assert.That(job.IsTerminated, Is.False);
+                Assert.That(job.IsTerminationRequested, Is.False);
+
+                cancellation.Cancel();
+                await task;
             }
         }
 
         public class BackgroundJobService
         {
-            public int Count { get; }
+            readonly Dictionary<string, BackgroundJob> jobs = 
+                 new Dictionary<string, BackgroundJob>();
 
-            public bool IsRunning(string id)
-            {
-                return false;
-            }
+            public int Count => jobs.Count;
+            public bool IsRunning(string id) => jobs.ContainsKey(id);
+            public BackgroundJob this[string id] => jobs.Find(id);
 
-            public BackgroundJob this[string id]
+            public BackgroundJob Run(string id, Func<BackgroundJobTerminationToken, Task> job)
             {
-                get { return null; }
+                var j = new BackgroundJob(job);
+                jobs.Add(id, j);
+                j.Run();
+                return j;
             }
         }
 
         public class BackgroundJob
+        {
+            readonly Func<BackgroundJobTerminationToken, Task> job;
+
+            internal BackgroundJob(Func<BackgroundJobTerminationToken, Task> job) => 
+                this.job = job;
+
+            public bool IsRunning()
+            {
+                return !IsTerminated();
+            }
+
+            public bool IsTerminated()
+            {
+                return false;
+            }
+
+            public bool IsTerminationRequested()
+            {
+                return false;
+            }
+
+            public void Run()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class BackgroundJobTerminationToken
         {
 
         }
